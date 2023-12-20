a483e0f9c2e37a20822745fcfcc329e67124b23b
Cwjiee <jackchong398@gmail.com>
2023-12-20 21:19:49 +0800

    fix: post controller not imported

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\api\V1\NewsController;
 use App\Http\Controllers\api\V1\PetController;
 use App\Http\Controllers\api\V1\ReportController;
 use App\Http\Controllers\api\v1\UserController;
+use App\Http\Controllers\api\V1\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -23,21 +24,21 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::group(['prefix' => 'v1'], function() {
-    
+Route::group(['prefix' => 'v1'], function () {
+
     // User routes
     Route::get('/users', [UserController::class, 'index']);
     Route::post('/register', [UserController::class, 'register'])->middleware('guest');
     Route::post('/login', [UserController::class, 'login'])->middleware('guest');
     Route::get('/users/{id}', [UserController::class, 'show']);
     Route::delete('/users/{id}', [UserController::class, 'destroy']);
-    
+
     Route::apiResource('/pets', PetController::class);
     Route::apiResource('/posts', PostController::class);
     Route::apiResource('/news', NewsController::class);
     Route::apiResource('/reports', ReportController::class);
-    
-    Route::group(['middleware' => ['auth:sanctum']], function() {
+
+    Route::group(['middleware' => ['auth:sanctum']], function () {
         Route::post('/logout', [UserController::class, 'logout']);
 
         //Appointment routes
@@ -45,6 +46,5 @@ Route::group(['prefix' => 'v1'], function() {
         Route::post('/appointments', [AppointmentController::class, 'store']);
         Route::put('/appointments/{aptid}', [AppointmentController::class, 'update']);
         Route::get('/appointments/{spid}', [AppointmentController::class, 'show']);
-    }); 
-}); 
-
+    });
+});
